f3d301f05a7a4f64375d444db3f73b014483d954
Rene Santiago <rsj510@gmail.com>
2023-02-04 23:20:08 -0500

    add game functionality and fix various function

--- poolcleaner.py
+++ poolcleaner.py
@@ -16,21 +16,47 @@ def clean_pool():
     
 def check_stats():
     tool = tools[game["tool"]]
-    print(f"You currently have {game['money']}and are using a {tool['name']}")
+    print(f"You currently have {game['money']} and are using a {tool['name']}")
     
 def upgrade():
+    if (game["tool"] >= len(tools) - 1):
+        print("no more upgrades")
+        return 0
     next_tool = tools[game["tool"]+1]
     if (next_tool == None):
         print("There is no more tools")
         return 0
-    if (game["money"] -= next_tool["cost"]):
-        print('not enough to buy tool')
+    if (game["money"] < next_tool["cost"]):
+        print('Not enough to buy tool')
         return 0
+    print("Ypu are upgrading your tool")
     game["money"] -= next_tool["cost"]
     game["tool"] += 1
 
 def win_check():
-    if(game['tool'] == 1 and game["money"] == 1000):
+    if(game['tool'] == 1 and game["money"] >= 1000):
         print("You Win")
         return True
     return False
+
+while (True):
+    
+    i = input("[1] Clean Pool [2] Check Stats [3] Upgrade [4] Quit")
+    
+    i = int(i)
+    
+    if (i == 1):
+        clean_pool()
+        
+    if (i == 2):
+        check_stats()
+        
+    if (i == 3):
+        upgrade()
+        
+    if (i == 4):
+        print("You quit the game")
+        break 
+    
+    if (win_check()):
+        break
