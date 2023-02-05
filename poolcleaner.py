f68ac60ceb164e61fcd472351c7415cf10fe0592
Rene Santiago <rsj510@gmail.com>
2023-02-04 23:04:21 -0500

    add clean pool/check_stats/upgrade function

---
+++ poolcleaner.py
@@ -0,0 +1,30 @@
+# Game State
+
+game = {"tool": 0, "money": 0}
+
+tools = [
+    {"name": "Small Net", "profit": 10, "cost": 0},
+    {"name": "Big Net", "profit": 100, "cost": 100},
+]
+
+# Game Option Function
+
+def clean_pool():
+    tool = tools[game["tool"]]
+    print(f"You clean a pool with your {tool['name']} and make {tool['profit']}")
+    game["money"] += tool["profit"]
+    
+def check_stats():
+    tool = tools[game["tool"]]
+    print(f"You currently have {game['money']}and are using a {tool['name']}")
+    
+def upgrade():
+    next_tool = tools[game["tool"]+1]
+    if (next_tool == None):
+        print("There is no more tools")
+        return 0
+    if (game["money"] -= next_tool["cost"]):
+        print('not enough to buy tool')
+        return 0
+    game["money"] -= next_tool["cost"]
+    game["tool"] += 1 
